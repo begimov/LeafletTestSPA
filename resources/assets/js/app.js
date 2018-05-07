f78c93986b5ef30c49e0847ad76ec1ef4788943d
begimov <begimov@gmail.com>
2018-05-07 12:00:49 +0300

    vuex store created and bootstraped

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,6 +9,8 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import store from './store'
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
