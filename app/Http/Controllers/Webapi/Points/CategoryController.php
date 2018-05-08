a6bb31825ee2a7384c5e96e099052cb964d10abc
begimov <begimov@gmail.com>
2018-05-08 08:16:52 +0300

    Webapi CategoryController has been created

---
+++ app/Http/Controllers/Webapi/Points/CategoryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Webapi\Points;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+}
