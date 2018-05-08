642570c2fc5a63e644c52b9382647f4306a5a561
begimov <begimov@gmail.com>
2018-05-08 08:17:39 +0300

    Webapi PointController has been created

---
+++ app/Http/Controllers/Webapi/Points/PointController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Webapi\Points;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PointController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+}
