070c18f3723d52e1000893cae3e1dd20f97b4341
begimov <begimov@gmail.com>
2018-05-08 09:46:38 +0300

    CategoryRepository Eloquent implementation pulled in CategoryController

--- app/Http/Controllers/Webapi/Points/CategoryController.php
+++ app/Http/Controllers/Webapi/Points/CategoryController.php
@@ -5,10 +5,19 @@ namespace App\Http\Controllers\Webapi\Points;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Repositories\Contracts\Points\CategoryRepository;
+
 class CategoryController extends Controller
 {
+    protected $categories;
+
+    public function __construct(CategoryRepository $categories)
+    {
+        $this->categories = $categories;
+    }
+
     public function index()
     {
-        //
+        dd($this->categories->get());
     }
 }
