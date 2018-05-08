3039e99a25c93c42fa0a45b7020c07f170da2d69
begimov <begimov@gmail.com>
2018-05-08 11:08:41 +0300

    accessor for POINT data type casting has been implemented in Point model class

--- app/Models/Points/Point.php
+++ app/Models/Points/Point.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Point extends Model
 {
-    //
+    public function getPositionAttribute($value)
+    {
+        $point = $this->selectRaw(('ST_X(position) AS lat, 
+            ST_Y(position) AS lng'))->where('id', $this->id)->first();
+
+        return [
+            'lat' => $point->lat,
+            'lng' => $point->lng
+        ];
+    }
 }
